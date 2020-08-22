346a2e3c71a0a53ca7a6c00c67fe85f955139e3a
Parth Raghav <iniento12@gmail.com>
2020-08-22 19:08:25 +0530

    adding get_resolved

--- server/analyze.py
+++ server/analyze.py
@@ -6,6 +6,11 @@ nlp = spacy.load('en')
 import neuralcoref
 neuralcoref.add_to_pipe(nlp)
 
+pronoun_list = [
+    'he', 'she', 'it', 'they', 'them', 'him', 'her', 'his', 'hers', 'its',
+    'we', 'us'
+]
+
 
 def get_sentence_at_index(index, resolved_list):
     """Returns the sentence beginning at the index
@@ -18,7 +23,7 @@ def get_sentence_at_index(index, resolved_list):
         if val in end_of_sentence_punctuation:
             break
         else:
-            begin_index += 1
+            begin_index -= 1
     while end_index <= len(resolved_list) - 1:
         val = resolved_list[end_index].strip()
         if val in end_of_sentence_punctuation:
@@ -28,10 +33,42 @@ def get_sentence_at_index(index, resolved_list):
     return resolved_list[begin_index + 1:end_index + 1], begin_index + 1
 
 
-def coref_distance():
-    """Returns a distance between a co-refering mention from the main mention.
+def get_resolved(doc, clusters):
+    """Returns list of utterances text where the co-refering mentions are resolved to their closest main mention
     """
-    pass
+    resolved = [tok.text_with_ws for tok in doc]
+    lines = []
+    for cluster in clusters:
+        for coref in cluster:
+            coref_text = coref.text.lower()
+            cluster_main_text = cluster.main.text.lower()
+
+            _isSingleToken = len(coref_text.split()) == 1
+            _isNotMainMention = coref_text != cluster_main_text
+            _isIdentifiablePronoun = coref_text in pronoun_list
+
+            if _isSingleToken and _isNotMainMention and _isIdentifiablePronoun:
+                # Token is pronoun!
+                original_sentence, start_index = get_sentence_at_index(
+                    coref.start, resolved)
+                processed_sentence = ''.join(original_sentence).lower()
+
+                if ' '.join(cluster_main_text.strip().split()
+                            ) not in processed_sentence:
+                    # The main mention does not occur in this sentence as a word
+                    # Resolve co-referring mention with the main mention
+                    resolved[coref.start] = cluster.main.text + doc[
+                        coref.end - 1].whitespace_
+                    # Capitalise the first letter of the name
+                    if start_index == coref.start:
+                        resolved[coref.start] = resolved[
+                            coref.start].capitalize()
+                    final_sentence = ''.join(original_sentence)
+
+                    if len(final_sentence) > 0:
+                        lines.append([
+                            final_sentence, coref.text, resolved[coref.start]
+                        ])
 
 
 def neutralise(doc):
@@ -49,6 +86,8 @@ def neutralise(doc):
 if __name__ == '__main__':
     # You're done. You can now use NeuralCoref as you usually manipulate a SpaCy document annotations.
     doc = nlp(
-        u'''During Destiny's Child's hiatus, Beyoncé made her theatrical film debut with a role in the US box-office number-one Austin Powers in Goldmember (2002) and began her solo music career. She became the first music act to debut at number one with their first six solo studio albums on the Billboard 200.[7] Her debut album Dangerously in Love (2003) featured four Billboard Hot 100 top five songs, including the number-one singles "Crazy in Love" featuring rapper Jay-Z and "Baby Boy" featuring singer-rapper Sean Paul. Following the disbandment of Destiny's Child in 2006, she released her second solo album, B'Day, which contained her first US number-one solo single "Irreplaceable", and "Beautiful Liar", which topped the charts in most countries. Beyoncé continued her acting career with starring roles in The Pink Panther (2006), Dreamgirls (2006), and Obsessed (2009). Her marriage to Jay-Z and her portrayal of Etta James in Cadillac Records (2008) influenced her third album, I Am... Sasha Fierce (2008), which earned a record-setting six Grammy Awards in 2010. It spawned the UK number-one single "If I Were a Boy", the US number-one single "Single Ladies (Put a Ring on It)" and the top five single "Halo".'''
+        u'''During Destiny's Child's hiatus, Beyoncé made her theatrical film debut with a role in the US box-office number-one Austin Powers in Goldmember (2002) and began her solo music career. She became the first music act to debut at number one with their first six solo studio albums on the Billboard 200.[7] Her debut album Dangerously in Love (2003) featured four Billboard Hot 100 top five songs, including the number-one singles "Crazy in Love" featuring rapper Jay-Z and "Baby Boy" featuring singer-rapper Sean Paul. She married Jay-Z. He cheated on him. Following the disbandment of Destiny's Child in 2006, she released her second solo album, B'Day, which contained her first US number-one solo single "Irreplaceable", and "Beautiful Liar", which topped the charts in most countries. Beyoncé continued her acting career with starring roles in The Pink Panther (2006), Dreamgirls (2006), and Obsessed (2009). Her marriage to Jay-Z and her portrayal of Etta James in Cadillac Records (2008) influenced her third album, I Am... Sasha Fierce (2008), which earned a record-setting six Grammy Awards in 2010. It spawned the UK number-one single "If I Were a Boy", the US number-one single "Single Ladies (Put a Ring on It)" and the top five single "Halo".'''
     )
-    neutralise(doc)
+    clusters = doc._.coref_clusters
+    # neutralise(doc)
+    get_resolved(doc, clusters)
