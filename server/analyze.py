c3f3c0762bf3257cf1a816c2bb7026fbcab4ff91
Parth Raghav <iniento12@gmail.com>
2020-08-22 21:44:42 +0530

    refactoring

--- server/analyze.py
+++ server/analyze.py
@@ -40,14 +40,12 @@ def get_sentence_at_index(index, resolved_list):
     return resolved_list[begin_index + 1:end_index + 1], begin_index + 1
 
 
-def get_resolved(doc, clusters):
-    """Returns list of utterances text where the co-refering mentions are resolved to their closest main mention
+def neutralise(doc, clusters):
+    """Neutralise gendered coreferences in a text block.
     """
     resolved = [tok.text_with_ws for tok in doc]
     lines = []
     for cluster in clusters:
-        print('\n')
-        print(cluster)
         for coref in cluster:
             coref_text = coref.text.lower()
             cluster_main_text = cluster.main.text.lower()
@@ -84,14 +82,15 @@ def get_resolved(doc, clusters):
                             detected_pronoun_set = SECOND_PERSONAL_PRONOUNS
                         elif token.tag_ == 'PRP' and 'obj' in token.dep_:
                             detected_pronoun_set = SECOND_OBJECTIVE_PRONOUNS
-                        elif token.tag_ == 'PRP$' and token.dep_ == 'poss':
+                        elif token.tag_ == 'PRP$' and token.dep_ in [
+                                'poss', 'nmod'
+                        ]:
                             detected_pronoun_set = SECOND_PROMINAL_PRONOUNS
                         elif token.tag_ == 'NNS' and token.dep_ == 'attr':
                             detected_pronoun_set = SECOND_POSSESSIVE_PRONOUNS
                         else:
-                            print("fuck", pronoun, pronoun
-                                  in SECOND_PERSONAL_PRONOUNS,
-                                  SECOND_PERSONAL_PRONOUNS)
+                            print("fuck", pronoun, token.tag_, token.dep_,
+                                  ''.join(original_sentence))
 
                         resolved[coref.start] = detected_pronoun_set[-1] + doc[
                             coref.end - 1].whitespace_
@@ -106,10 +105,7 @@ def get_resolved(doc, clusters):
                             for word in original_sentence
                         ]
                     final_sentence = ''.join(original_sentence)
-    print('\n\n\n\n\n')
-    print(''.join(resolved))
-    for line in lines:
-        print(line[1:])
+    return ''.join(resolved)
 
 
 def coref_distance(coref, main):
@@ -118,23 +114,10 @@ def coref_distance(coref, main):
     return abs(coref.start - main.end)
 
 
-def neutralise(doc):
-    """Neutralise gendered coreferences in a text block.
-    """
-    plural_lemma_tags = {"NNS", "NNPS"}
-    for token in doc:
-        lemma = token.text
-        if token.tag_ in plural_lemma_tags:
-            # Token is plural
-            print(lemma)
-            pass
-
-
 if __name__ == '__main__':
     # You're done. You can now use NeuralCoref as you usually manipulate a SpaCy document annotations.
     doc = nlp(
         u'''During Destiny's Child's hiatus, Beyoncé made her theatrical film debut with a role in the US box-office number-one Austin Powers in Goldmember (2002) and began her solo music career. She became the first music act to debut at number one with their first six solo studio albums on the Billboard 200.[7] Her debut album Dangerously in Love (2003) featured four Billboard Hot 100 top five songs, including the number-one singles "Crazy in Love" featuring rapper Jay-Z and "Baby Boy" featuring singer-rapper Sean Paul. She married Jay-Z. He cheated on her. Following the disbandment of Destiny's Child in 2006, she released her second solo album, B'Day, which contained her first US number-one solo single "Irreplaceable", and "Beautiful Liar", which topped the charts in most countries. Beyoncé continued her acting career with starring roles in The Pink Panther (2006), Dreamgirls (2006), and Obsessed (2009). Her marriage to Jay-Z and her portrayal of Etta James in Cadillac Records (2008) influenced her third album, I Am... Sasha Fierce (2008), which earned a record-setting six Grammy Awards in 2010. It spawned the UK number-one single "If I Were a Boy", the US number-one single "Single Ladies (Put a Ring on It)" and the top five single "Halo".'''
     )
     clusters = doc._.coref_clusters
-    # neutralise(doc)
-    get_resolved(doc, clusters)
+    print(neutralise(doc, clusters))
