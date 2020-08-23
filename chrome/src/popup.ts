2f57cf603b20c09e8f866ab161af993b29154555
Parth Raghav <iniento12@gmail.com>
2020-08-23 08:14:30 +0530

    modified popup.ts

--- chrome/src/popup.ts
+++ chrome/src/popup.ts
@@ -1,32 +1,56 @@
-import * as moment from 'moment';
-import * as $ from 'jquery';
+import * as moment from "moment";
+import * as $ from "jquery";
 
 let count = 0;
 
-$(function() {
+var tabStatusMap = {};
+
+function handleWindowLoad() {
+  console.log("i was loaded");
+}
+
+$(function () {
+  handleWindowLoad();
+
   const queryInfo = {
     active: true,
-    currentWindow: true
+    currentWindow: true,
   };
 
-  chrome.tabs.query(queryInfo, function(tabs) {
-    $('#url').text(tabs[0].url);
-    $('#time').text(moment().format('YYYY-MM-DD HH:mm:ss'));
-  });
-
-  chrome.browserAction.setBadgeText({text: count.toString()});
-  $('#countUp').click(()=>{
-    chrome.browserAction.setBadgeText({text: (++count).toString()});
-  });
-
-  $('#changeBackground').click(()=>{
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {
-        color: '#555555'
+  chrome.tabs.query(queryInfo, function (tabs) {
+    // $('#url').text(tabs[0].url);
+    // $('#time').text(moment().format('YYYY-MM-DD HH:mm:ss'));
+    if (!Object.keys(tabStatusMap).includes(tabs[0].id.toString())) {
+      tabStatusMap[tabs[0].id] = false;
+    }
+    chrome.tabs.sendMessage(
+      tabs[0].id,
+      {
+        isNeutralised: tabStatusMap[tabs[0].id],
       },
-      function(msg) {
-        console.log("result message:", msg);
-      });
+      function (msg) {
+        console.log(msg);
+      }
+    );
+    tabStatusMap[tabs[0].id] = true;
+  });
+
+  chrome.browserAction.setBadgeText({ text: count.toString() });
+  $("#countUp").click(() => {
+    chrome.browserAction.setBadgeText({ text: (++count).toString() });
+  });
+
+  $("#changeBackground").click(() => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(
+        tabs[0].id,
+        {
+          color: "#555555",
+        },
+        function (msg) {
+          console.log("result message:", msg);
+        }
+      );
     });
   });
 });
